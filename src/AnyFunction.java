19f0a5e25666d1a71fdd876e5f006222a3caed56
N1S4H <joanna.radom12@gmail.com>
2025-09-17 14:10:35 +0200

    krok 8

---
+++ src/AnyFunction.java
@@ -0,0 +1,22 @@
+public class AnyFunction implements CalculatedFunction{
+    private CalculatedFunction funkcjadowykonania;
+
+    @Override
+    public double f(double x) {
+        return funkcjadowykonania.f(x);
+    }
+
+    public AnyFunction(CalculatedFunction funkcja) {
+        this.funkcjadowykonania = funkcja;
+    }
+
+    public static void main(String[] args) {
+        AnyFunction sin = new AnyFunction(x -> Math.sin(x)*2);
+        AnyFunction cos = new AnyFunction(x -> Math.cos(x)*2);
+        AnyFunction sinPodwojny = new AnyFunction(x-> sin.f(x) * cos.f(x));
+
+
+        System.out.println(sin.f(6));
+        System.out.println(sinPodwojny.f(6));
+    }
+}
